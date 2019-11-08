2d479eae467be9cb76426d16da467775dc636134
Bryan <bryanleon_2687@hotmail.com>
2019-11-08 23:22:44 +0000

    cambio10

--- lab2/devops/Jenkinsfile_desa.groovy
+++ lab2/devops/Jenkinsfile_desa.groovy
@@ -32,6 +32,7 @@ try {
             tenantId=spcredential[2]
             subscrId=spcredential[3]
             sh "cp lab2/docker/Dockerfile ."
+            sh "ls -lta"
 
           sh = "docker build -t az-demo:1.0 --build-arg --build-arg CLIENT_ID=${clientId} --build-arg CLIENT_SECRET=${clientse} --build-arg TENANT_ID=${tenantId} --build-arg SUBSCRIPTION_ID=${subscrId} ."
          sh "docker images"
