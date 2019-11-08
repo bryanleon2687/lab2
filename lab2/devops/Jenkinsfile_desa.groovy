6aaf826adf5ca75ae9bfbdbddf5ebb0a01b5fc15
Bryan <bryanleon_2687@hotmail.com>
2019-11-08 22:36:53 +0000

    cambios3

--- lab2/devops/Jenkinsfile_desa.groovy
+++ lab2/devops/Jenkinsfile_desa.groovy
@@ -9,15 +9,26 @@ try {
           dir("source"){
               git(
                   branch : "master",
-                  credentialsId: "opando-github",
+                  credentialsId: "github",
                   url: "https://github.com/opando/aks-rbac-example.git"
               )
           }
       }
       stage('Build Container') {      
          
-          
-     
+         
+         withCredentials([string(credentialsId:"bleon-sp",variable:'azcre')]){
+            //token : CLIENT_ID|CLIENT_SECRET|TENANT_ID|SUBSCRIPTION_ID
+
+            spcre = azcre.split('\\|')
+            clientId=spcredential[0]     
+            clientse=spcredential[1] 
+            tenantId=spcredential[2]
+            subscrId=spcredential[3]
+
+          sh = "docker build -t az-demo:1.0 --build-arg --build-arg CLIENT_ID=${clientId} --build-arg CLIENT_SECRET=${clientse} --build-arg TENANT_ID=${tenantId} --build-arg SUBSCRIPTION_ID=${subscrId}"
+
+
    }
    
 } 
